4570f632d18a7fc844718582f79030d5809525c6
Thomas Jandecka <thomas.jandecka@oracle.com>
2018-11-07 07:52:35 +0100

    ci: finished setting up all checks

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -2,19 +2,17 @@ cmake_minimum_required(VERSION 3.6)
 set(CTEST_PROJECT_NAME "Drafter")
 set(CTEST_BUILD_NAME "GNU/coverage")
 
-set(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
-set(CTEST_BINARY_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/build")
+set(CTEST_SOURCE_DIRECTORY ".")
+set(CTEST_BINARY_DIRECTORY "build")
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 set(CTEST_USE_LAUNCHERS 1)
 
 set(CTEST_COVERAGE_COMMAND "/usr/bin/gcov")
-set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
 
 set(Drafter_CONFIG_OPTIONS
     "-DCMAKE_CXX_FLAGS=--coverage"
-    "-DCMAKE_BUILD_TYPE=Debug"
     "-DINTEGRATION_TESTS=ON"
     )
 
@@ -23,5 +21,4 @@ ctest_configure(OPTIONS "${Drafter_CONFIG_OPTIONS}")
 ctest_build(FLAGS -j5)
 ctest_test()
 ctest_coverage(CAPTURE_CMAKE_ERROR COVERAGE_ERROR)
-ctest_memcheck(EXCLUDE DrafterIntegration)
 ctest_submit()
--- cmake/gnu_debug.cmake
+++ cmake/gnu_debug.cmake
@@ -1,18 +1,16 @@
 cmake_minimum_required(VERSION 3.5)
 set(CTEST_PROJECT_NAME "Drafter")
-set(CTEST_BUILD_NAME "GNU/release")
+set(CTEST_BUILD_NAME "GNU/debug")
 
-set(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
-set(CTEST_BINARY_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/build")
+set(CTEST_SOURCE_DIRECTORY ".")
+set(CTEST_BINARY_DIRECTORY "build")
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 set(CTEST_USE_LAUNCHERS 1)
 
-set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
-
 set(Drafter_CONFIG_OPTIONS
-    "-DCMAKE_BUILD_TYPE=Release"
+    "-DCMAKE_BUILD_TYPE=Debug"
     "-DINTEGRATION_TESTS=ON"
     )
 
@@ -20,5 +18,4 @@ ctest_start("Continuous")
 ctest_configure(OPTIONS "${Drafter_CONFIG_OPTIONS}")
 ctest_build(FLAGS -j5)
 ctest_test()
-ctest_memcheck(EXCLUDE DrafterIntegration)
 ctest_submit()
