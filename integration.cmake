867d8f4f6d67bb051c5e98199758ba74feada5d8
Thomas Jandecka <thomas.jandecka@oracle.com>
2018-11-08 04:45:48 +0100

    build: avoid using find_program for bundler

--- integration.cmake
+++ integration.cmake
@@ -1,9 +1,7 @@
 cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
 
-find_program(Bundler bundle)
-
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/features DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
 
-add_custom_target(BundleInstall ALL COMMAND ${Bundler} install --path vendor/bundle)
-add_test(NAME DrafterIntegration COMMAND ${Bundler} exec cucumber)
+add_custom_target(BundleInstall ALL COMMAND bundle install --path vendor/bundle)
+add_test(NAME DrafterIntegration COMMAND bundle exec cucumber)
 set_tests_properties(DrafterIntegration PROPERTIES ENVIRONMENT "DRAFTER_BINARY_DIR=${Drafter_BINARY_DIR}")
